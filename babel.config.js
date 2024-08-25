1c35ffd29e97e5464eeff772db410569da99b52d
daviaxs <cadwvk@gmail.com>
2024-08-25 11:22:12 -0300

    chore: Add module-resolver plugin for path aliases

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,23 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    "plugins": [
+      [
+        "module-resolver",
+        {
+          root: "./src",
+          alias: {
+            '@routes': './src/routes',
+            '@screens': './src/screens',
+            '@storage': './src/storage',
+            '@utils': './src/shared/utils',
+            '@theme': './src/shared/theme',
+            '@assets': './src/shared/assets',
+            '@components': './src/shared/components',
+          }
+        }
+      ]
+    ]
   };
 };
