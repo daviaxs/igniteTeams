b4b6257e40dadd76444cfcc0e8006c393b157bb8
daviaxs <cadwvk@gmail.com>
2024-08-25 17:46:04 -0300

    feat: Add header list with team filter and number of players in Players screen

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,11 +1,16 @@
-import { Header } from "@components/header";
-import { Container, Form } from "./styles";
-import { Highlight } from "@components/highlight";
-import { ButtonIcon } from "@components/button-icon";
-import { Input } from "@components/input";
-import { Filter } from "@components/filter";
+import { Header } from "@components/header"
+import { Container, Form, HeaderList, NumberOfPlayers } from "./styles"
+import { Highlight } from "@components/highlight"
+import { ButtonIcon } from "@components/button-icon"
+import { Input } from "@components/input"
+import { Filter } from "@components/filter"
+import { useState } from "react"
+import { FlatList } from "react-native"
 
 export function Players() {
+  const [team, setTeam] = useState('Time a')
+  const [players, setPlayers] = useState<string[]>(['Davi', "henrique", "matheus"])
+
   return (
     <Container>
       <Header showBackButton />
@@ -16,7 +21,7 @@ export function Players() {
       />
 
       <Form>
-        <Input 
+        <Input
           placeholder="Nome da pessoa"
           autoCorrect={false}
         />
@@ -26,7 +31,24 @@ export function Players() {
         />
       </Form>
 
-      <Filter title="Time a" isActive />
+      <HeaderList>
+        <FlatList
+          horizontal
+          data={['Time a', 'Time b']}
+          keyExtractor={(_, index) => String(index)}
+          renderItem={({ item }) => (
+            <Filter
+              title={item}
+              isActive={item === team}
+              onPress={() => setTeam(item)}
+            />
+          )}
+        />
+
+        <NumberOfPlayers>
+          {players.length}
+        </NumberOfPlayers>
+      </HeaderList>
     </Container>
   )
 }
