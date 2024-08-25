5fb03502401d350c28a11497aaa1cf9d7e1ba51f
daviaxs <cadwvk@gmail.com>
2024-08-25 11:59:51 -0300

    feat: add theme

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { Groups } from '@screens/Groups';
+import { ThemeProvider } from 'styled-components/native';
+import theme from '@theme/index'
 
 export default function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Groups />
       <StatusBar style="light" />
-    </>
+    </ThemeProvider>
 
   );
 }
