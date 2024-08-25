1e8b74e9f63948dd38cc20291d6b476cce6569b7
daviaxs <cadwvk@gmail.com>
2024-08-25 15:34:20 -0300

    feat: Add ListEmpty component and update Groups screen

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,9 +4,10 @@ import { Highlight } from '@components/highlight';
 import { GroupCard } from '@components/group-card';
 import { useState } from 'react';
 import { FlatList } from 'react-native';
+import { ListEmpty } from '@components/list-empty';
 
 export function Groups() {
-  const [groups, setGroups] = useState<string[]>(['Galera do Ignite', 'Turma do Trabalho', 'Amigos da Faculdade', 'Família']);
+  const [groups, setGroups] = useState<string[]>([]);
 
   return (
     <Container>
@@ -17,6 +18,10 @@ export function Groups() {
       <FlatList
         data={groups}
         keyExtractor={(_, index) => String(index)}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message='Não há turmas cadastradas.' />
+        )}
         renderItem={({ item }) => (
           <GroupCard title={item} />
         )}
