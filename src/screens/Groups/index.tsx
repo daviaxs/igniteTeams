59b843e95cc3072bdd40df4b5842c3fc989ce8d4
daviaxs <cadwvk@gmail.com>
2024-08-25 15:23:22 -0300

    feat: Update Groups screen to use FlatList for rendering group cards

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -2,15 +2,25 @@ import { Header } from '@components/header';
 import { Container } from './styles';
 import { Highlight } from '@components/highlight';
 import { GroupCard } from '@components/group-card';
+import { useState } from 'react';
+import { FlatList } from 'react-native';
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(['Galera do Ignite', 'Turma do Trabalho', 'Amigos da Faculdade', 'Família']);
+
   return (
     <Container>
       <Header showBackButton />
 
       <Highlight title='Turmas' subtitle='Jogue com sua turma' />
 
-      <GroupCard title='Turma do Ignite' />
+      <FlatList
+        data={groups}
+        keyExtractor={(_, index) => String(index)}
+        renderItem={({ item }) => (
+          <GroupCard title={item} />
+        )}
+      />
     </Container>
   );
 }
