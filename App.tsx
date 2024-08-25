a9cdbc5a4f186ad7ddcc79293e21ec4a1da86780
daviaxs <cadwvk@gmail.com>
2024-08-25 13:23:00 -0300

    feat: add Loading component

--- App.tsx
+++ App.tsx
@@ -1,22 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { ThemeProvider } from 'styled-components/native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
-import { ActivityIndicator } from 'react-native';
 
 import { Groups } from '@screens/Groups';
 import theme from '@theme/index'
+import { Loading } from '@components/loading';
 
 export default function App() {
   const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold})
 
-  if (!fontsLoaded) {
-    return <ActivityIndicator />
-  }
-
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
-      <StatusBar style="light" />
+      {fontsLoaded ? <Groups /> : <Loading />}
+      <StatusBar style="light" translucent />
     </ThemeProvider>
 
   );
