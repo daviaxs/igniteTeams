91dc5abb7b9840e60b3c83ba4bba16d11c138d2c
daviaxs <cadwvk@gmail.com>
2024-08-25 14:20:50 -0300

    feat: Update Header component to include a back button

--- src/shared/components/header/index.tsx
+++ src/shared/components/header/index.tsx
@@ -1,9 +1,21 @@
-import { Container, Logo } from "./styles";
+import { BackButton, BackIcon, Container, Logo } from "./styles";
 import logoImg from "@assets/logo.png"
 
-export function Header() {
+interface HeaderProps {
+  showBackButton?: boolean
+}
+
+export function Header({showBackButton = false}: HeaderProps) {
   return (
-    <Container>
+    <Container hasBackButton={showBackButton}>
+      {
+        showBackButton && (
+          <BackButton>
+            <BackIcon />
+          </BackButton>
+        )
+      }
+
       <Logo source={logoImg} />
     </Container>
   )
--- src/shared/components/header/styles.ts
+++ src/shared/components/header/styles.ts
@@ -1,10 +1,15 @@
+import { ChevronLeft } from "lucide-react-native"
 import styled from "styled-components/native"
 
-export const Container = styled.View`
+interface ContainerProps {
+  hasBackButton: boolean
+}
+
+export const Container = styled.View<ContainerProps>`
   width: 100%;
 
   flex-direction: row;
-  justify-content: center;
+  justify-content: ${({hasBackButton}) => hasBackButton ? "space-between" : "center"};
   align-items: center;
 `
 
@@ -12,3 +17,11 @@ export const Logo = styled.Image`
   width: 46px;
   height: 55px;
 `
+
+export const BackButton = styled.TouchableOpacity`
+`
+
+export const BackIcon = styled(ChevronLeft).attrs(({theme}) => ({
+  color: theme.COLORS.WHITE,
+  size: 36
+}))``
