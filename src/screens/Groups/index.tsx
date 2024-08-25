69ccc8b17865c465bd8046527f9bb8d84067f584
daviaxs <cadwvk@gmail.com>
2024-08-25 14:46:12 -0300

    feat: Add Highlight component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,13 @@
 import { Header } from '@components/header';
 import { Container } from './styles';
+import { Highlight } from '@components/highlight';
 
 export function Groups() {
   return (
     <Container>
       <Header showBackButton />
+
+      <Highlight title='Turmas' subtitle='Jogue com sua turma' />
     </Container>
   );
 }
