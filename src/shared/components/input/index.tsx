d0fc9dc95fc2262f608cab103645e7efee7aeef8
daviaxs <cadwvk@gmail.com>
2024-08-26 16:05:30 -0300

    refactor: Update Input component to accept a ref for TextInput

--- src/shared/components/input/index.tsx
+++ src/shared/components/input/index.tsx
@@ -1,12 +1,18 @@
-import { TextInputProps } from "react-native";
+import { TextInput, TextInputProps } from "react-native";
 import { InputStyle } from "./styles";
+import { RefObject } from "react";
 
 interface InputProps extends TextInputProps {
+  inputRef?: RefObject<TextInput>
   placeholder?: string
 }
 
-export function Input({placeholder, ...rest}: InputProps) {
+export function Input({placeholder, inputRef, ...rest}: InputProps) {
   return (
-    <InputStyle placeholder={placeholder} {...rest} />
+    <InputStyle 
+      placeholder={placeholder} 
+      ref={inputRef} 
+      {...rest} 
+    />
   )
 }
