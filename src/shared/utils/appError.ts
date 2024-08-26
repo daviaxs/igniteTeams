79e104fd94d19be2c95cfc18fe1a1c4546333b91
daviaxs <cadwvk@gmail.com>
2024-08-26 14:22:09 -0300

    feat: Add appError class for handling custom errors

---
+++ src/shared/utils/appError.ts
@@ -0,0 +1,7 @@
+export class appError {
+  message: string
+
+  constructor(message: string) {
+    this.message = message
+  }
+}
--- src/storage/groups/groupCreate.ts
+++ src/storage/groups/groupCreate.ts
@@ -1,13 +1,21 @@
 import AsyncStorage from "@react-native-async-storage/async-storage"
 import { GROUP_COLLECTION } from "@storage/storageConfig"
 import { groupsGetAll } from "./groupsGetAll"
+import { appError } from "@utils/appError"
 
 export async function groupCreate(groupName: string) {
   try {
     const storedGroups = await groupsGetAll()
 
+    const groupAlreadyExists = storedGroups.includes(groupName)
+
+    if (groupAlreadyExists) {
+      throw new appError('Já existe um grupo cadastrado com esse nome.')
+    }
+
     const storage = JSON.stringify([...storedGroups, groupName])
     await AsyncStorage.setItem(GROUP_COLLECTION, storage)
+    
   } catch (error) {
     throw error
   }
